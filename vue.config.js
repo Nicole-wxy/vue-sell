c37ea75dce8cbd67d3bab453b0bcced193558491
wuxiaoying <1121900174@qq.com>
2024-02-01 22:06:16 +0800

    更新编译文件

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-sell/' : '/',
   css: {
     loaderOptions: {
       stylus: {
