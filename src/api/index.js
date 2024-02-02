abab08e3bd149a47b2d4be6221fa946bbbaa62b1
wuxiaoying <1121900174@qq.com>
2024-02-02 18:17:33 +0800

    .

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import { get } from './helpers'
-const getSeller = get('/api/seller')
-const getGoods = get('/api/goods')
-const getRatings = get('/api/ratings')
+const getSeller = get('./api/seller')
+const getGoods = get('./api/goods')
+const getRatings = get('./api/ratings')
 export {
     getSeller,
     getGoods,
