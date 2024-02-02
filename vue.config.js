16cf9e8ee198cbd7dcbf8a0385e466e0c877a7a6
wuxiaoying <1121900174@qq.com>
2024-02-02 11:42:26 +0800

    json地址修改

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 const webpack = require('webpack')
 const path = require('path')
-const appData = require('./data.json')
-const seller = appData.seller
-const goods = appData.goods
-const ratings = appData.ratings
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
+const appData = require(resolve('/data.json'))
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-sell/' : '/',
   css: {
@@ -27,20 +28,20 @@ module.exports = {
   },
   devServer: {
     before(app) {
-      app.get('/vue-sell/api/seller', (req, res) => {
+      app.get('/api/seller', (req, res) => {
         const id = req.query.id
         res.json({
           errno: 0,
           data: Object.assign({}, seller, {id})
         })
       })
-      app.get('/vue-sell/api/goods', (req, res) => {
+      app.get('/api/goods', (req, res) => {
         res.json({
           errno: 0,
           data: goods
         })
       })
-      app.get('/vue-sell/api/ratings', (req, res) => {
+      app.get('/api/ratings', (req, res) => {
         res.json({
           errno: 0,
           data: ratings
