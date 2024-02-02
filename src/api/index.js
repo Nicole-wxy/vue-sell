b27d9c3734237182a1eb8fcf935073053fe9f88b
wuxiaoying <1121900174@qq.com>
2024-02-02 18:39:46 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,11 @@
 import { get } from './helpers'
-const getSeller = get('./api/seller')
-const getGoods = get('./api/goods')
-const getRatings = get('./api/ratings')
+let baseUrl = './'
+if (process.env.NODE_ENV === 'production') {
+    baseUrl = '/vue-sell/'
+}
+const getSeller = get(baseUrl + 'api/seller')
+const getGoods = get(baseUrl + 'api/goods')
+const getRatings = get(baseUrl + 'api/ratings')
 export {
     getSeller,
     getGoods,
